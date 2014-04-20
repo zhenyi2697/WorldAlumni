fcfef611a746eec062d88a6b0427777502e1701b
zhenyi2697 <zhenyi2697@gmail.com>
2014-04-20 02:44:07 -0400

    Added google map support

--- static/js/query.js
+++ static/js/query.js
@@ -51,27 +51,54 @@
     $("#loading-message").hide();
   }
 
+  var mapOpened = false;
+  var mapInitiated = false;
+  var toggleMap = function(){
+    if(mapOpened) {
+      $("#friend-map").slideUp();
+      mapOpened = false;
+      $("#showMap").html("<i class='fa fa-search'></i> open map");
+    }else {
+      $("#friend-map").slideDown(function(){
+        if (!mapInitiated) {
+          $("#friend-map").gmap3();
+          mapInitiated = true;
+          refreshMap();
+        }
+      });
+      mapOpened = true;
+      $("#showMap").html("<i class='fa fa-search'></i> close map");
+    }
+  }
+
+  var markers = [];
   var load_friends = function(friends){
 
     var rows = [];
     var row_data = "";
     var f;
+    var context_data = "";
 
     if (friends.length <= 1) {
       $("#friend-message").html("It seems that you are the only one around here, come back later!");
     } else {
-      $("#friend-message").html("We have found "+ (friends.length - 1) +" schoolmates for you");
+      var html = "We have found "+ (friends.length - 1) +" schoolmates for you";
+      html += "<span class='label label-warning text-small' id='showMap' onclick='toggleMap();' style='margin-left:10px;cursor:pointer;'><i class='fa fa-search'></i> open map</span>"
+      $("#friend-message").html(html);
     }
+
 
     for (var i = 0; i < friends.length; i++) {
       f = friends[i];
       row_data = "<tr>";
       if (f.provider == 'facebook') {
-        row_data += "<td><img src='"+ f.image_url +"?width=40&height=40' alt='"+f.firstName+"' class='img-circle friend-thumbnail'/></td>";
+        row_data += "<td><img src='"+ f.image_url +"?width=40&height=40' alt='"+f.first_name+"' class='img-circle friend-thumbnail'/></td>";
         row_data += "<td><a href='http://www.facebook.com/"+ f.uid  +"' target='_blank'>"+ f.first_name + " "+ f.last_name +"</a></td>";
+        context_data = "<img src='"+ f.image_url +"?width=24&height=24' class='img-circle friend-thumbnail' style='margin-right:5px;'/><a href='http://www.facebook.com/"+ f.uid  +"' target='_blank'>" + f.first_name + " " + f.last_name + "</a>";
       } else {
         row_data += "<td><img src='"+ f.image_url + "' style='width:40px;height:40px;' class='img-circle friend-thumbnail' /></td>";
         row_data += "<td>"+ f.first_name + " "+ f.last_name +"</td>";
+        context_data = "<img src='"+ f.image_url + "' style='width:24px;height:24px;margin-right:5px;' class='img-circle friend-thumbnail'/>" + f.first_name + " " + f.last_name;
       }
 
       if (f.associated_attendances.length > 0 && f.attendances.length > 0 ) {
@@ -85,9 +112,59 @@
       row_data += "<td class='visible-xs'>"+ f.distance +" ("+ f.appear_time +")</td>";
 
       rows.push(row_data);
+
+      //gps data
+      if (f.latitude != "0" && f.longitude != "0"){
+        var gpsdata = {
+          data: context_data,
+          latLng: [f.latitude, f.longitude],
+          id: f.bindingId
+        }
+        markers.push(gpsdata);
+      }
+
     }
     var html = rows.join();
     $("#friend-table").html(html);
+
+    if (mapInitiated) {
+      refreshMap();
+    }
+
+  }
+
+  var refreshMap = function(){
+
+    // clear old markers
+	  $("#friend-map").gmap3({
+	  	clear:{name:"marker"}
+	  });
+
+    // init new markers
+    $("#friend-map").gmap3({
+        marker: {
+            values: markers,
+	          events:{
+	            click: function(marker, event, context){
+	            	var map = this.gmap3("get"),
+	  	          infowindow = this.gmap3({get:{name:"infowindow"}});
+	  	          if (infowindow){
+	  	            infowindow.close();
+	  	            infowindow.setContent(context.data);
+	  	            infowindow.open(map, marker);
+	  	          } else {
+	  	            this.gmap3({
+	  	              infowindow:{
+	  	                anchor:marker,
+	  	                options:{content: context.data }
+	  	              }
+	  	            });
+	  	          }
+	            }
+	          }
+        }
+    });
+  
   }
 
   var user_nearby = function(){
